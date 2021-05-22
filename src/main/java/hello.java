55d693027258de955f8278a87a035b40c7e1622d
nuoztu <nuoztu@gmail.com>
2021-05-22 15:04:49 -0500

    added more hello feature

--- src/main/java/hello.java
+++ src/main/java/hello.java
@@ -2,5 +2,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("developing hello future");
+        System.out.println("github repo is already published");
+        System.out.println("doing work");
     }
 }
